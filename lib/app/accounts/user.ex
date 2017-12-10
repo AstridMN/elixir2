86e91cbb9cd5bfda0713e8893fed871afc62059b
iyoo14 <iyoo14@gmail.com>
2017-12-10 19:07:35 +0900

    first commit

---
+++ lib/app/accounts/user.ex
@@ -0,0 +1,20 @@
+defmodule App.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias App.Accounts.User
+
+
+  schema "users" do
+    field :age, :integer
+    field :name, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(%User{} = user, attrs) do
+    user
+    |> cast(attrs, [:name, :age])
+    |> validate_required([:name, :age])
+  end
+end
